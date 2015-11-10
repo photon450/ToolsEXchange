d1a97d2dade6099a70cbd90865fb4f2f20b0f309
photon450 <dnspena@gmail.com>
2015-11-10 17:09:28 -0500

    Updates pages

--- app/controllers/UserPage.java
+++ app/controllers/UserPage.java
@@ -9,12 +9,12 @@ import play.*;
 import play.mvc.*;
 
 import views.html.*;
+
 import play.data.Form;
 import play.db.ebean.Model;
 import static play.libs.Json.toJson;
 import java.util.List;
 
 public class UserPage extends Controller {
-    public Result getUserPage() {return ok(views.html.UserPage.render());
-    }
+    public Result getUserPage() {return ok(views.html.UserPage.render()); }
 }
