1516a658ffda26a664857ecc3bf9c4fdb7e4d0df
photon450 <dnspena@gmail.com>
2015-12-18 10:23:51 -0500

    Added responsiveness and new navbar

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -4,10 +4,13 @@ import play.mvc.*;
 
 import views.html.*;
 
+
+
 public class Application extends Controller {
 
     public Result index() {
         return ok(Home.render());
     }
+    public Result getAbout() {return ok(About.render());}
 
 }
--- build.sbt
+++ build.sbt
@@ -29,3 +29,5 @@ fork in run := true
 fork in run := true
 
 fork in run := true
+
+fork in run := true
